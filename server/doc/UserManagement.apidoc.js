795fcf35435b3225a5f919c0d3370e1c93098de5
Matthieu-CM <matthieu.correia-moreira@epitech.eu>
2020-01-06 18:19:24 +0100

    [ADD] body-parser + cors [ADD] files for the apidoc

---
+++ server/doc/UserManagement.apidoc.js
@@ -0,0 +1,90 @@
+/**
+ * @apiDefine UserLoginError
+ *
+ * @apiError DoesntExist The account doesn't exists in the database
+ *
+ * @apiErrorExample {String} Doesnt Exist
+ *     HTTP/1.1 400 Bad Request
+ *     "Your account doesn't exists"
+ *
+ * @apiError BadPassword The password doesn't match with the one stored for this account
+ *
+ * @apiErrorExample {String} Bad Password
+ *     HTTP/1.1 400 Bad Request
+ *     "PASSWORD ERROR"
+ */
+
+/**
+ * @apiDefine UserRegistrationError
+ *
+ * @apiError UserExist The Username is already taken
+ *
+ * @apiErrorExample {String} Cant Update
+ *     HTTP/1.1 400 Bad Request
+ *     "User John already exists"
+ *
+ * @apiError EmailExist The Email is already associated to an account
+ *
+ * @apiErrorExample {String} Email Exist
+ *     HTTP/1.1 400 Bad Request
+ *     "An email is already associate with an account"
+ */
+
+/**
+ * @apiDefine UserDeleteError
+ *
+ * @apiError  CantFindAccount The account to delete cannot be found
+ *
+ * @apiErrorExample {String} Cant Find Account
+ *     HTTP/1.1 400 Not Modified
+ *     "Could not find the account"
+ *
+ * @apiError AnotherAccount The account to delete wasn't matching with the user own access_token
+ *
+ * @apiErrorExample {String} Another Account
+ *     HTTP/1.1 403 Forbidden
+ *     "You can only delete your account"
+ */
+
+/**
+ * @api {post} /login Log an User
+ * @apiGroup UserManagement
+ *
+ * @apiParam {String} username username of the account (in the body)
+ * @apiParam {String} password Password of the account (In the body)
+ *
+ * @apiSuccessExample User Loged
+ *     HTTP/1.1 200
+ *
+ * @apiUse UserLoginError
+ */
+
+/**
+* @api {post} /register Register a User
+* @apiDescription Create an account for the user
+* @apiGroup UserManagement
+*
+* @apiParam {Number} username (In the body) Username of the user
+* @apiParam {String} password (In the body) Password of the user
+* @apiParam {String} email (In the body) Email to associate with the account
+*
+* @apiSuccessExample {String} User Created
+*     HTTP/1.1 200
+*      "Account created"
+*
+* @apiUse UserRegistrationError
+*/
+
+/**
+* @api {delete} /user/:email/:token Delete a User
+* @apiDescription delete an User account
+* @apiGroup UserManagement
+*
+* @apiParam {String} token User own token
+* @apiParam {String} email email of the account to delete
+*
+* @apiSuccessExample {String} User Deleted
+*     HTTP/1.1 200
+*      "User deleted"
+* @apiuse UserDeleteError
+*/
---
+++ server/doc/doc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-11-14T10:26:26.584Z",
+    "url": "http://apidocjs.com",
+    "version": "0.18.0"
+  }
+});
