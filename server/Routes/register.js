77765f84584e346040e24a10b77b55396cf304da
Matthieu-CM <matthieu.correia-moreira@epitech.eu>
2020-01-07 20:40:48 +0100

    [ADD] test [FIX] register route

--- server/Routes/register.js
+++ server/Routes/register.js
@@ -5,15 +5,22 @@ var sha256 = require('sha256');
 module.exports = function (app, db) {
     app.post("/register", function (req, res) {
         // console.log(req.body)
-        if (req.body.username === "" || req.body.password === "") {
+        if (req.body.username === "" || req.body.password === "" || req.body.email === "") {
             res.status(400).send("You can't send an empty field")
             return;
         }
         db.collection("users").find({username: req.body.username}).toArray(function(err, result) {
             if (result[0] === undefined) {
                 password = sha256(PREFIX_SALT + req.body.password + SUFFIX_SALT)
-                db.collection("users").insert(req.body)
-                console.log(password)
+                let userToken = sha256(req.body.username + new Date().getTime())
+                let insertion = {
+                    username: req.body.username,
+                    password: password,
+                    email: req.body.email,
+                    userToken: userToken
+                }
+                db.collection("users").insert(insertion)
+                res.set("UserToken", userToken)
                 res.status(200).send("User created")
             } else {
                 res.status(400).send("This user already exists")
