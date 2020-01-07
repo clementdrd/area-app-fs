e52db300409a4590357e5a915d730739ec5782f9
Matthieu-CM <matthieu.correia-moreira@epitech.eu>
2020-01-07 13:01:48 +0100

    [ADD] latest documentation [ADD] CI

--- server/app.js
+++ server/app.js
@@ -5,14 +5,16 @@ var bodyParser = require('body-parser')
 var cors = require("cors")
 
 // parse application/x-www-form-urlencoded
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 
-const myUrl = 'mongodb://localhost:27017';
+const myUrl = 'mongodb+srv://AREA:AREA@users-uxyki.mongodb.net/test?retryWrites=true&w=majority';
 
 MongoClient.connect(myUrl, function (err, db) {
     db = db.db("AREA")
 
+    console.log("TOTO")
     require("./Routes/register")(app, db)
     require("./Routes/isonline")(app, db)
     require("./Routes/login")(app, db)
@@ -20,6 +22,17 @@ MongoClient.connect(myUrl, function (err, db) {
     app.use(function (req, res, next) {
         res.status(404).send('404 page not found');
     });
+    app.listen(8080, function() {
+        app.emit('listening', null)
+        console.log("Server is up and running on port 8080")
+    })
+
+
+    app.get('/closeServer', function(req, res) {
+        res.send(200)
+        server.close();
+    })
 })
 
-app.listen(8080)
+
+module.exports = app
