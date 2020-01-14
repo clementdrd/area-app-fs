02e29110111ee203a103e48bda59d7f59191aee0
Matthieu-CM <matthieu.correia-moreira@epitech.eu>
2020-01-14 13:44:17 +0100

    [ADD] doc to api

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ var app = express()
 const MongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser')
 var cors = require("cors")
+var engine = require('consolidate');
+const path = require('path');
 
 // parse application/x-www-form-urlencoded
 
@@ -10,6 +12,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 
 const myUrl = 'mongodb+srv://AREA:AREA@users-uxyki.mongodb.net/test?retryWrites=true&w=majority';
+
+
 
 MongoClient.connect(myUrl, { useUnifiedTopology: true }, function (err, db) {
     db = db.db("AREA")
@@ -20,12 +24,30 @@ MongoClient.connect(myUrl, { useUnifiedTopology: true }, function (err, db) {
     require("./Routes/deleteUser")(app, db)
     require("./Routes/createAdmins")(app, db)
 
+
+    let dirname = __dirname + "/mydoc"
+    //add the router
+    app.use(express.static(dirname));
+
+    app.engine('html', engine.mustache);
+    app.set('view engine', 'html');
+    
+    app.get("/doc", (req, res) => {
+        console.log(dirname + "/index.html")
+        res.sendFile(dirname + "/index.html")
+    })
+
     app.use(function (req, res, next) {
         res.set("Content-Type", "text/html")
         res.status(404).send('404 page not found');
     });
-    app.listen(process.env.PORT || 8080, function() {
+
+
+    let port = process.env.PORT || 8080;
+
+    app.listen(port, () => {
         app.emit('listening', null)
+        console.log("Server is up and running on port ", port)
     })
 
 })
