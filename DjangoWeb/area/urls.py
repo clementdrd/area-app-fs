93e6d92d4dacb9839e0232ce0e24b5c9f4e4ed0d
Souhib <strabelsi@abbana.com>
2020-01-15 09:34:56 +0100

    add static file path on django webApp

--- DjangoWeb/area/urls.py
+++ DjangoWeb/area/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'area'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.home, name='home')
 ]
--- DjangoWeb/area/views.py
+++ DjangoWeb/area/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.views import generic
 from django.template import loader
 from django.http import HttpResponse
+import requests
 # Create your views here.
 
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    response = requests.get('https://reqres.in/api/users?page=2')
+    geodata = response.json()
+    return render(request, 'home.html', {
+        'ip': geodata,
+    })
