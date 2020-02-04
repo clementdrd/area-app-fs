a9646e26ef63c3db1b8f30691ea019e337a941d0
Matthieu <matthieu.correia-moreira@epitech.eu>
2020-02-04 14:37:08 +0100

    [FIX] deleting an account also delete the tokens

--- server/Routes/deleteUser.js
+++ server/Routes/deleteUser.js
@@ -10,11 +10,13 @@ module.exports = function (app, db) {
                     res.status(403).send("You are not allowed to do this request")
                 } else if (result[0].admin === true) {
                     db.collection("users").find({username: req.body.userToDelete}).toArray(function(er, accountToDelete) {
-                        db.collection("users").deleteOne({_id: accountToDelete[0]._id})
+                        db.collection("users").deleteOne({ _id: accountToDelete[0]._id })
+                        db.collection("tokens").deleteOne({ userToken: accountToDelete[0].userToken })
                     })
                     res.status(200).send("User " + req.body.userToDelete + " deleted by admin")
                 } else if (req.body.username === result[0].username) {
-                    db.collection("users").deleteOne({_id: result[0]._id})
+                    db.collection("users").deleteOne({ _id: result[0]._id })
+                    db.collection("tokens").deleteOne({ userToken: result[0].userToken })
                     res.status(200).send("User " + req.body.username + " deleted")
                 } else {
                     res.status(403).send("You are not allowed to modify another user account")
