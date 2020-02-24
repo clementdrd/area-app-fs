6bcacd6373e449dc9bef7f710fe6d7fee3a52f4c
Souhib <strabelsi@abbana.com>
2020-02-24 17:48:19 +0100

    add nasa action with dropbox reaction

--- server/Functions/dropbox.js
+++ server/Functions/dropbox.js
@@ -1,6 +1,8 @@
 const fetch = require("node-fetch");
 const dropboxV2Api = require('dropbox-v2-api');
-const token = require("../Functions/DribbleFunctions")
+var Token = require("../Functions/getToken")
+var fs = require('fs');
+var request = require('request');
 
 module.exports = function (app, db) {
     app.get('/send_nasa_pic_to_dropbox', function (req, res) {
@@ -8,14 +10,18 @@ module.exports = function (app, db) {
             res.status(401).send("Unauthorized")
         }
         else {
-            NasaFileHistory(req.headers.usertoken)
+            // let token = new Token(db)
+            db.collection("tokens").find({userToken: req.headers.usertoken}).toArray((err, result) => {
+                NasaFileHistory(result[0].dropbox)
+            })
             res.status(200).send('Activated');
         }
     })
 }
 
-function NasaFileHistory(usertoken)
+function NasaFileHistory(access_token)
 {
+    console.log(access_token)
     var url = "https://api.nasa.gov/planetary/apod?api_key=6MpjO3T3rsOcQTm1JX8ah4JtL23PEVhfJW1t6PXG"
     
     fetch(url, { method: 'GET'})
@@ -23,30 +29,41 @@ function NasaFileHistory(usertoken)
         return res.json()
     })
     .then((json) => {
-    console.log(json);
-    PrepareMail(json, usertoken);
+        console.log(json.url)
+        SendPictureToDropbox(json, access_token);
     }).catch((err) => {console.log(err)});
 }
 
-function PrepareMail(json, usertoken)
-{
-    // let mail = new Mail
-    // let test = "This is the daily Image of the nasa\n"
-    var url = "https://area-rest-api-zuma.herokuapp.com/getAccessToken?usertoken=" + usertoken + "&servicename=dropbox"
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+}
 
-    fetch(url, { method: 'GET'})
-    .then(res => {
-        return res.json()
-    })
-    .then((json) => {
-        console.log(json);
-    })
-    // const dropbox = dropboxV2Api.authenticate({
-    //     token: 'your token'
+function SendPictureToDropbox(json, usertoken, access_token)
+{
+    var headers = {
+        'Authorization': 'Bearer ' + access_token,
+        'Content-Type': 'application/json'
+    };
+    var dataString = '{"path": "/picture_of_the_day.jpg","url": "' + json.url + '"}';
+    console.log(dataString)
+    
+    var options = {
+        url: 'https://api.dropboxapi.com/2/files/save_url',
+        method: 'POST',
+        headers: headers,
+        body: dataString
+    };
+    request(options, callback);
+    // dropbox({
+    //     resource: 'files/upload',
+    //     parameters: {
+    //         path: json.url
+    //     },
+    //     readStream: fs.createReadStream(json.url)
+    // }, (err, result, response) => {
+    //     if (err) { return console.log(err); }
+    //     console.log(result);
     // });
-    // console.log(json.url)
-    // db.collection("users").find({userToken: token}).toArray(function(err, result)
-    // {
-    //     mail.sendEmail(result[0].email, "AREA image of the day Nasa",json.explanation + "\n\n"+  json.hdurl);
-    // })
 }
--- server/Functions/getToken.js
+++ server/Functions/getToken.js
@@ -1,14 +1,15 @@
 class Token {
     constructor(db) {
         this.db = db;
+        this.value;
     }
 
-    getToken(serviceName, userToken) {
-        this.db.collection("tokens").find({
+    async getToken(serviceName, userToken) {
+        return await this.db.collection("tokens").find({
             userToken: userToken
-        }).toArray(function (err, result) {
+        }).toArray((err, result) => {
             if (result[0] === undefined) {
-                return({data: "Could not find an account that matches the token"})
+                return ({data: "Could not find an account that matches the token"})
             } else {
                 let serviceValue = result[0][serviceName.toLowerCase()]
                 if (serviceValue === undefined) {
@@ -18,8 +19,9 @@ class Token {
                 }
             }
         })
+        // console.log("My return = ", myreturn)
+        // console.log("Value = ", this.value)
     }
-
 }
 
 module.exports = Token;
