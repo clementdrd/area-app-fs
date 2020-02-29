74a358a3f473a1d703636575d3d0083f269b2144
Souhib <strabelsi@abbana.com>
2020-02-29 19:08:02 +0100

    [FIX] body to headers in Activated.js

--- server/Routes/Activated.js
+++ server/Routes/Activated.js
@@ -2,11 +2,11 @@ module.exports = function (app, db) {
     app.get("/isActivated", (req, res) => {
         if (!req.headers.usertoken) {
             db.collection("users").find({
-                userToken: req.body.userToken
+                userToken: req.headers.userToken
             }).toArray((err, result) => {
                 if (result[0] === undefined || result[0] === null) {
                     db.colletion("Services").find({
-                        userToken: req.body.userToken
+                        userToken: req.headers.userToken
                     }).toArray((error, services) => {
                         res.set("Content-Type", "application/json")
                         res.status(200).send(services)
