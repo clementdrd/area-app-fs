602711228fd1b4c0e068d52b29c77ce3d8378846
Souhib <strabelsi@abbana.com>
2020-02-29 19:21:10 +0100

    [FIX] Activated route on server

--- server/Routes/Activated.js
+++ server/Routes/Activated.js
@@ -1,12 +1,12 @@
 module.exports = function (app, db) {
     app.get("/isActivated", (req, res) => {
-        if (!req.headers.usertoken) {
+        if (req.headers.usertoken) {
             db.collection("users").find({
-                userToken: req.headers.userToken
+                userToken: req.headers.usertoken
             }).toArray((err, result) => {
-                if (result[0] === undefined || result[0] === null) {
-                    db.colletion("Services").find({
-                        userToken: req.headers.userToken
+                if (result[0] !== undefined || result[0] !== null) {
+                    db.collection("Services").find({
+                        userToken: req.headers.usertoken
                     }).toArray((error, services) => {
                         res.set("Content-Type", "application/json")
                         res.status(200).send(services)
