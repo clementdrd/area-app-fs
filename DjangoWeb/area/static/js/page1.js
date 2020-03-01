031fb8b50c970cad13cbc285e5ea5fc7819884e8
Souhib <strabelsi@abbana.com>
2020-03-01 14:20:44 +0100

    add dict with all routes on DjangoWeb

--- DjangoWeb/area/static/js/page1.js
+++ DjangoWeb/area/static/js/page1.js
@@ -1,4 +1,33 @@
-const usertoken = getCookie('userToken')
+const reaction = {
+    "dropbox + nasa": "/send_nasa_pic_to_dropbox",
+    "nasa + dropbox": "/send_nasa_pic_to_dropbox",
+
+    "nasa + mail": "/nasadaily",
+
+    "dropbox + imgur": "/send_best_img_pic_to_dropbox",
+    "imgur + dropbox": "/send_best_img_pic_to_dropbox",
+
+    "nasa + imgur": "/imgurnasadaily",
+    "imgur + nasa": "/imgurnasadaily",
+
+    "dribble + imgur": "/dribblepost",
+    "imgur + dribble": "/dribblepost",
+
+    "imgur + imgur": "/ImgurFavorites",
+
+    "spotify + mail": "/spotifyresume",
+
+    "spotify + spotify": "/spotifyhistory",
+
+    "trello + gitlab": "/trelloGitlabOrga",
+    "gitlab + trello": "/trelloGitlabOrga",
+
+    "pushbullet + standings": "/premier_league_schedule_sms",
+    "standings + pushbullet": "/premier_league_schedule_sms",
+
+    "pushbullet + upcomingMatch": "/upcoming_match",
+    "upcomingMatch + pushbullet": "/upcoming_match",
+}
 
 function getCookie(cname) {
     var name = cname + "=";
--- DjangoWeb/area/static/js/service_connection.js
+++ DjangoWeb/area/static/js/service_connection.js
@@ -2,15 +2,17 @@ const services = ["spotify", "weather", "netflix", "gmail",
                   "dropbox", "deezer",
                   "twitter", "facebook", "amazon", "google-calendar",
                   "google-drive", "instagram", "messenger", "microsoft-outlook",
-                  "nasa", "souncloud", "twitch", "trello", "pushbullet", "imgur", "football"]
+                  "nasa", "souncloud", "twitch", "trello", "pushbullet", "imgur", "football", "gitlab", "dribble"]
 const tab = {
-    "dropbox": [{"name" : "dropbox + nasa", "state" : "false"},{"name" : "dropbox + imgur", "state" : "false"}],
-    "spotify": [{"name" : "spotify + mail", "state" : "false"},{"name" : "spotify + spotify", "state" : "false"}],
-    "trello": [{"name" : "trello + gitlab", "state" : "false"}],
-    "imgur": [{"name" : "imgur + dropbox", "state" : "false"},{"name" : "imgur + TOTO", "state" : "false"}],
-    "nasa": [{"name" : "nasa + dropbox", "state" : "false"},{"name" : "nasa + mail", "state" : "false"}, {"name" : "nasa + imgur", "state" : "false"}],
-    "football": [{"name" : "football + pushbullet", "state" : "false"},{"name" : "football + pushbullet", "state" : "false"}],
-    "pushbullet": [{"name" : "pushbullet + football", "state" : "false"},{"name" : "pushbullet + football", "state" : "false"}],
+    "dropbox": [{"name" : "dropbox + nasa"},{"name" : "dropbox + imgur"}],
+    "spotify": [{"name" : "spotify + mail"},{"name" : "spotify + spotify"}],
+    "trello": [{"name" : "trello + gitlab"}],
+    "imgur": [{"name" : "imgur + dropbox"},{"name" : "imgur + TOTO"}],
+    "nasa": [{"name" : "nasa + dropbox"},{"name" : "nasa + mail"}, {"name" : "nasa + imgur"}],
+    "football": [{"name" : "standings + pushbullet"},{"name" : "upcomingMatch + pushbullet"}],
+    "pushbullet": [{"name" : "pushbullet + standings"},{"name" : "pushbullet + upcomingMatch"}],
+    "gitlab": [{"name" : "gitlab + trello"}],
+    "dribble": [{"name" : "dribble + imgur"}],
 }
 
 function init(app_name) {
@@ -24,22 +26,29 @@ function init(app_name) {
     document.getElementById('button1').innerHTML = mydata[0]["name"];
     
     var button_1_url = (mydata[0]["name"]).split(" ").join("_");
-    var button_2_url = (mydata[1]["name"]).split(" ").join("_");
+    if (app_name !== "gitlab" || app_name !== "trello")
+        var button_2_url = (mydata[1]["name"]).split(" ").join("_");
 
     var url1 = button_1_url.split("_+_");
-    var url2 = button_2_url.split("_+_");
+    if (app_name !== "gitlab" || app_name !== "trello")
+        var url2 = button_2_url.split("_+_");
 
 
     var goto_button1 = "window.location.href='/Page/?" + "service=" + url1[0] + "&service2=" + url1[1] + "'";
-    var goto_button2 = "window.location.href='/Page/?" + "service=" + url2[0] + "&service2=" + url2[1] + "'";
+    if (app_name !== "gitlab" || app_name !== "trello")
+        var goto_button2 = "window.location.href='/Page/?" + "service=" + url2[0] + "&service2=" + url2[1] + "'";
 
     var foo = document.getElementById("button1");
     foo.setAttribute("onclick", goto_button1);
 
     var foo = document.getElementById("button2");
-    foo.setAttribute("onclick", goto_button2);
-
-    document.getElementById('button2').innerHTML = mydata[1]["name"];
+    if (app_name !== "gitlab" || app_name !== "trello") {
+        foo.setAttribute("onclick", goto_button2);
+        document.getElementById('button2').innerHTML = mydata[1]["name"];
+    }
+    else {
+        document.getElementById('button2').style.display = "none";
+    }
 }
 
 function change_logo() {
