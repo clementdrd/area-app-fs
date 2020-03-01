dac7e9c0220a56ade6f8678fb0f4131b81523a38
Matthieu <matthieu.correia-moreira@epitech.eu>
2020-03-01 16:32:08 +0100

    [ADD] the different Auth

--- DjangoWeb/area/static/js/handleAuth.js
+++ DjangoWeb/area/static/js/handleAuth.js
@@ -1,16 +1,22 @@
 const usertoken = getCookie('userToken')
 
+const redirect_uri = "http://" + "127.0.0.1" + ":8000/";
+
 const redirect_link = {
-    "spotify": "https://accounts.spotify.com/authorize?client_id=f6349b82adab4d12a42520ae5f530830&redirect_uri=http:%2F%2Flocalhost:8000%2F&scope=user-read-currently-playing%20user-read-email&response_type=token&state=123",
-    "twitch": "https://id.twitch.tv/oauth2/authorize?client_id=5nnkmcrbqaxrv1irgdqpten4rcnhr2&redirect_uri=http:%2F%2Flocalhost:8000%2F&response_type=token&scope=user:edit",
-    "dropbox": "https://www.dropbox.com/oauth2/authorize?client_id=yp5wl24jy9d6l8l&response_type=token&redirect_uri=http:%2F%2Flocalhost:8000%2F",
-    "pushbullet": "https://www.pushbullet.com/authorize?client_id=R3ame1lmBzYYVzkQ9IvaVhF1XtRucYJQ&redirect_uri=http%3A%2F%2Flocalhost%3A8000&response_type=token&scope=everything"
+    "spotify": "https://accounts.spotify.com/authorize?client_id=f6349b82adab4d12a42520ae5f530830&redirect_uri=" + redirect_uri + "&scope=user-read-currently-playing%20user-read-email&response_type=token&state=123",
+    // "twitch": "https://id.twitch.tv/oauth2/authorize?client_id=5nnkmcrbqaxrv1irgdqpten4rcnhr2&redirect_uri=" + redirect_uri + "&response_type=token&scope=user:edit",
+    "dropbox": "https://www.dropbox.com/oauth2/authorize?client_id=yp5wl24jy9d6l8l&response_type=token&redirect_uri=" + redirect_uri,
+    "pushbullet": "https://www.pushbullet.com/authorize?client_id=R3ame1lmBzYYVzkQ9IvaVhF1XtRucYJQ&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000&response_type=token&scope=everything",
+    "imgur": "https://api.imgur.com/oauth2/authorize?client_id=6c756bc671ff8b6&response_type=token",
+    "gitlab": "https://gitlab.com/oauth/authorize?client_id=211e20b122dc89a8593c8b86ea7bd02b1242d09e264b8c9947577b8929f5a727&redirect_uri=" + redirect_uri + "&response_type=code&state=123&scope=api%20sudo",
+    "trello": "https://trello.com/1/authorize/?key=51fa8f3fa712867ca344ee0bb04b1de0&name=area&response_type=token&expiration=never&callback_method=fragment&return_url=" + redirect_uri + "&scope=read,write",
+    "dribble": "https://dribbble.com/oauth/authorize?client_id=f5f8a1979f1e7dc4cb0a06a8199e9b36db59a3f66733bff33ad44163a0d1e154&redirect_uri=" + redirect_uri + "&scope=public+upload&state=123"
 }
 
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
@@ -18,13 +24,13 @@ function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
         if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
+            return c.substring(name.length, c.length);
         }
     }
     return "";
@@ -33,12 +39,12 @@ function getCookie(cname) {
 function checkCookie() {
     var user = getCookie("username");
     if (user != "") {
-      alert("Welcome again " + user);
+        alert("Welcome again " + user);
     } else {
-      user = prompt("Please enter your name:", "");
-      if (user != "" && user != null) {
-        setCookie("username", user, 365);
-      }
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
     }
 }
 
@@ -49,13 +55,13 @@ function delete_cookie(name) {
 function findGetParameter(parameterName) {
     var result = null,
         tmp = [];
-        location.search
-            .substr(1)
-            .split("&")
-            .forEach(function (item) {
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
             tmp = item.split("=");
             if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-            });
+        });
     return result;
 }
 
@@ -66,16 +72,17 @@ function checkService() {
         if (service in redirect_link) {
             $.ajax({
                 url: "https://area-rest-api-zuma.herokuapp.com/getAllServices",
-                headers: {"usertoken": usertoken},
+                headers: {
+                    "usertoken": usertoken
+                },
                 success: function (result) {
                     console.log(result)
                     console.log(service)
                     try {
                         if (!result['Service'].includes(service)) {
-                            Access_Token(redirect_link[service])                        
+                            Access_Token(redirect_link[service])
                         }
-                    }
-                    catch {
+                    } catch {
                         Access_Token(redirect_link[service])
                     }
                 },
@@ -90,45 +97,101 @@ function checkService() {
 function Access_Token(lien) {
     service = findGetParameter('service')
     let access_token
-    let spotify_auth = window.open(
+    console.log(service === "gitlab")
+    if (service === "gitlab") {
+        gitlabOauth(lien, service);
+    } else {
+        let Popup = window.open(
+            lien,
+            service + " auth",
+            "resizable,scrollbars,status,location=yes,height=600,width=800,_blank",
+        )
+        var interval = setInterval(() => {
+            try {
+                if (Popup.closed) {
+                    clearInterval(interval)
+                } else {
+                    console.log(Popup.location)
+                    var regex = /[?#&]([^=#]+)=([^&#]*)/g,
+                        params = {},
+                        match;
+                    while (match = regex.exec(Popup.location)) {
+                        params[match[1]] = match[2];
+                    }
+                    console.log("PARAMS = ", params)
+                    if (params.token !== undefined) {
+                        params.access_token = params.token;
+                    }
+                    if (params.access_token === undefined && params.code === undefined) {
+                        if ((service === "dropbox" && params.code === undefined) || service !== "dropbox") {
+                            Popup.close()
+                            clearInterval(interval)
+                        }
+                    } else {
+                        Popup.close()
+                        clearInterval(interval)
+                        access_token = params.access_token
+                        console.log("usertoken: " + usertoken)
+                        console.log("servicename: " + service)
+                        console.log("value: " + access_token)
+                        $.post("https://area-rest-api-zuma.herokuapp.com/addAccessToken", {
+                            "usertoken": usertoken,
+                            "servicename": service,
+                            "value": access_token
+                        }).done(function (response) {
+                            console.log(response)
+                        });
+                    }
+                }
+            } catch (err) {
+                console.log(err)
+                // console.log("Params = ", params)
+            }
+        }, 1000)
+    }
+}
+
+
+function gitlabOauth(lien, service) {
+    let Popup = window.open(
         lien,
         service + " auth",
         "resizable,scrollbars,status,location=yes,height=600,width=800,_blank",
     )
     var interval = setInterval(() => {
         try {
-            if (spotify_auth.closed) {
+            if (Popup.closed) {
                 clearInterval(interval)
             } else {
+                let url = JSON.stringify(Popup.location.href)
                 var regex = /[?#&]([^=#]+)=([^&#]*)/g,
                     params = {},
                     match;
-                while (match = regex.exec(spotify_auth.location)) {
+                while (match = regex.exec(url)) {
                     params[match[1]] = match[2];
                 }
-                if (params.access_token === undefined && params.code === undefined){
-                    if ((service === "dropbox" && params.code === undefined) || service !== "dropbox") {
-                        spotify_auth.close()
-                        clearInterval(interval)
-                    }
+                if (params.code === undefined) {
+                    // Do Nothing
                 } else {
-                    spotify_auth.close()
-                    clearInterval(interval)
-                    access_token = params.access_token
-                    console.log("usertoken: " + usertoken)
-                    console.log("servicename: " + service)
-                    console.log("value: " + access_token)
-                    $.post( "https://area-rest-api-zuma.herokuapp.com/addAccessToken", 
-                    {
-                        "usertoken": usertoken, "servicename": service, "value": access_token
-                    }).done(function(response) {
+                    let code = params.code.substring(0, params.code.length - 1)
+                    let getToken = "https://gitlab.com/oauth/token?client_id=211e20b122dc89a8593c8b86ea7bd02b1242d09e264b8c9947577b8929f5a727&redirect_uri=" + redirect_uri + "&code=" + code + "&grant_type=authorization_code"
+                    console.log(getToken);
+                    fetch(getToken, {method : "POST", mode: "cors"})
+                    // $.post(getToken).done(function (response) {
+                    //     console.log(response)
+                    // });
+                    $.post("https://area-rest-api-zuma.herokuapp.com/addAccessToken", {
+                        "usertoken": usertoken,
+                        "servicename": service,
+                        "value": access_token
+                    }).done(function (response) {
+                        Popup.close()
                         console.log(response)
-                    });
+                    })
                 }
             }
-        } catch (err) {
-            console.log(params)
-            console.log(err);
+        } catch {
+            //TODO
         }
     }, 1000)
 }
